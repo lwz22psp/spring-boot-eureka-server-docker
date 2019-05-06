6e25ce7e962a957ee27844f38f62fbc8909b090a
Dark <lwz22psp@hotmail.com>
2019-05-06 15:23:34 +0800

    Revert "add zuul"
    This reverts commit ec52a065169a45dbcdcb8d73b2ecdfe9a4a9b61f.

--- src/main/java/hapiniu/demo/eurekadocker/EurekadockerApplication.java
+++ src/main/java/hapiniu/demo/eurekadocker/EurekadockerApplication.java
@@ -3,11 +3,9 @@ package hapiniu.demo.eurekadocker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaServer
 @SpringBootApplication
-@EnableZuulProxy
 public class EurekadockerApplication {
 
     public static void main(String[] args) {
